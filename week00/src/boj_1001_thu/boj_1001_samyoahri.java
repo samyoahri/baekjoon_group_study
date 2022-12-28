72ca2b00f61eb92be79cb904c7d0c104a647f921
samyoahri <samyoahri@gmail.com>
2022-12-28 17:11:48 +0900

    Create boj_1001_samyoahri

---
+++ week00/src/boj_1001_thu/boj_1001_samyoahri.java
@@ -0,0 +1,15 @@
+package boj_1001_thu;
+
+import java.util.Scanner;
+
+public class boj_1001_samyoahri {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A-B);
+
+        in.close();
+    }
+}
