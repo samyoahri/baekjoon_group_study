6a88456e3c972fe730ed82ed450fcbe671d955fb
khj1999 <mo481872@gmail.com>
2023-01-02 14:38:29 +0900

    Update boj_17068_khj1999.cpp

--- week02/src/boj_17068_mon_bronze/boj_17068_khj1999.cpp
+++ week02/src/boj_17068_mon_bronze/boj_17068_khj1999.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int arr[100001];
 
 int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
     int n, top = 0, cnt = 0;
     cin >> n;
     for(int i = 0; i < n; i++) cin >> arr[i];
