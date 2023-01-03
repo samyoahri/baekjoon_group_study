acae38d6687b1d63cc4af5a1787b6c1aff9a971f
khj1999 <mo481872@gmail.com>
2023-01-04 02:05:42 +0900

    Update boj_10773_khj1999.cpp

--- week02/src/boj_10773_wed/boj_10773_khj1999.cpp
+++ week02/src/boj_10773_wed/boj_10773_khj1999.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-	int k, ans = 0;
+    int k, ans = 0;
     cin >> k;
     vector<int> v(k);
 	for (int i = 0; i < k; i++) {
         cin >> v[i];
-		if (v[i] == 0) {
-			for (int j = i; true; j--) {
-				if (v[j] != 0) { v[j] = 0; break; }
+        if (v[i] == 0) {
+            for (int j = i; true; j--) {
+                if (v[j] != 0) { v[j] = 0; break; }
 			}
 		}
 	}
