c95b3a8f15e408b36548f94e936104b503662ee0
noachilles <kej010803@gmail.com>
2023-01-13 13:41:10 +0900

    Update boj_14713_noachilles

--- week03/src/Thu_boj_14713/boj_14713_noachilles.py
+++ week03/src/Thu_boj_14713/boj_14713_noachilles.py
@@ -9,17 +9,22 @@ if __name__ == "__main__":
         parrot_in.append(deque(map(str, input().strip().split())))
     parrot_out = deque(map(str, input().strip().split()))
     while parrot_out:
-        chk = False
-        word = parrot_out[0]
+        chkout = False
         for i in range(n):
-            if parrot_in[i] and word == parrot_in[i][0]:
+            if parrot_in[i] and parrot_out[0] == parrot_in[i][0]:
                 parrot_out.popleft()
                 parrot_in[i].popleft()
-                chk = True
+                chkout = True
                 break
-        if not chk:
+        if not chkout:
             break
-    if parrot_out:
+    chkin = True
+    for i in range(n):
+        if parrot_in[i]:
+            chkin = False
+        else:
+            continue
+    if parrot_out or not chkin:
         print('Impossible')
     else:
         print('Possible')
