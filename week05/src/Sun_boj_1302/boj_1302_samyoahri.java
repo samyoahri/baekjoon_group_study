0b80ca5ead2c813ea07dc236775ae82702c4b247
samyoahri <samyoahri@gmail.com>
2023-01-29 21:34:01 +0900

    create boj_1302_samyoahri

---
+++ week05/src/Sun_boj_1302/boj_1302_samyoahri.java
@@ -0,0 +1,47 @@
+package Sun_boj_1302;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_1302_samyoahri {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder ans = new StringBuilder();
+
+        Map<String, Integer> bestSeller = new HashMap<>();
+
+        int N = Integer.parseInt(in.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String bookName = in.readLine();
+            if (!bestSeller.containsKey(bookName)) {
+                bestSeller.put(bookName, 1);
+            }
+            else{
+                bestSeller.put(bookName, bestSeller.get(bookName)+1);
+            }
+        }
+        String str = "";
+        int max = -2147000000;
+        for (Map.Entry<String, Integer> entry : bestSeller.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            if (value > max) {
+                max = value;
+                str = entry.getKey();
+            } else if (value == max) {
+                if (str.compareTo(key) > 0) {
+                    str = entry.getKey();
+                }
+            }
+        }
+        ans.append(str);
+
+        out.write(ans.toString());
+        out.flush();
+
+        in.close();
+        out.close();
+    }
+}
