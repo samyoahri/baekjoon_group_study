777215018a26c828f14a31051655b3e38a06cc12
samyoahri <samyoahri@gmail.com>
2023-01-29 18:18:08 +0900

    Create boj_22252_samyoahri

--- week05/src/Free/boj_22252_samyoahri.java
+++ week05/src/Free/boj_22252_samyoahri.java
@@ -1,2 +1,27 @@
-package Free;public class boj_22252_samyoahri {
+package Free;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj_22252_samyoahri {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder ans = new StringBuilder();
+
+        StringTokenizer st;
+
+        int Q = Integer.parseInt(in.readLine());
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(in.readLine(), " ");
+
+//            int commandNumber
+        }
+
+        out.write(ans.toString());
+        out.flush();
+
+        in.close();
+        out.close();
+    }
 }
