8da61b64b59c7beff8c10426ead55ec3e07bc218
mcsoya <soya1994@naver.com>
2023-01-29 12:49:31 +0900

    Update boj_2910_mcsoya.cpp

--- weak05/src/Sun_boj_2910/boj_2910_mcsoya.cpp
+++ weak05/src/Sun_boj_2910/boj_2910_mcsoya.cpp
@@ -21,6 +21,8 @@ int main() {
 	}
 
 	vector<pair<int, int>> copy_vector(maps.begin(), maps.end());
+	// [=] : 스코프 내의 모든 변수 값을 복사 
+	// [&] : 스코프 내의 모든 변수를 "참조"로 복사
 	sort(copy_vector.begin(), copy_vector.end(), [&](pair<int, int>a, pair<int, int>b) {
 		if (a.second > b.second) return 1;
 		else if ((a.second == b.second) && (maps_idx[a.first] < maps_idx[b.first])) return 1;
