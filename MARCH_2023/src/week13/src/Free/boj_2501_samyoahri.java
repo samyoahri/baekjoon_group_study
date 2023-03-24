1e2311b1b8bed8286e49e476584189cb681319d3
samyoahri <samyoahri@gmail.com>
2023-03-24 23:46:06 +0900

    Update boj_2501_samyoahri

--- MARCH_2023/src/week13/src/Free/boj_2501_samyoahri.java
+++ MARCH_2023/src/week13/src/Free/boj_2501_samyoahri.java
@@ -1,6 +1,10 @@
 package Free;
 
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class boj_2501_samyoahri {
     public static void main(String[] args) throws IOException {
@@ -11,14 +15,19 @@ public class boj_2501_samyoahri {
         String[] strings = in.readLine().split(" ");
         int N = Integer.parseInt(strings[0]);
         int K = Integer.parseInt(strings[1]);
-        //약수의 개수
-        int cnt = 0;
-        for (int i = 1; i <= K; i++) {
-            if (N % K == 0) {
-                cnt++;
+        //약수의 배열
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                arrayList.add(i);
             }
         }
-        ans.append(cnt);
+        try {
+            ans.append(arrayList.get(K - 1));
+        } catch (Exception e) {
+            ans.append("0");
+        }
+
 
         out.write(ans.toString());
         out.flush();
