b4b8207cec72ee36e4090b08af44c03b4d2590d6
sjinwoo <sjw5953@naver.com>
2023-08-01 17:36:40 +0900

    Update boj_9935_sjinwoo

--- July_2023/src/week27/src/boj_9935_Sun_gold/boj_9935_sjinwoo.py
+++ July_2023/src/week27/src/boj_9935_Sun_gold/boj_9935_sjinwoo.py
@@ -10,8 +10,13 @@ def main():
         lst.append(char)
         if "".join(lst[-len(bomb):]) == bomb:
             for _ in bomb: lst.pop()
+
     if lst == []: print("FRULA")
     else: print("".join(lst))
+
+    # 영목이형 보시오. 이래도 파이썬 안해?
+    # lst = ['A', 'c', 4]
+    # print(", ".join(list(map(str, lst))))
         
 if __name__ == "__main__":
     main()
