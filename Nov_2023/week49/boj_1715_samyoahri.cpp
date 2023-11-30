cf7e32225abb0f6eab73bfa94fc6cd2a1e4ebe97
samyoahri <samyoahri@gmail.com>
2023-11-30 12:09:36 +0900

    Create boj_1715_samyoahri

---
+++ Nov_2023/week49/boj_1715_samyoahri.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+// #include<deque>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(0);
+    int N = 0;
+    cin >> N;
+    vector<int> arr(N);
+    priority_queue<int,vector<int>, greater<int>> cardsDequeASC;
+    for(int i = 0; i < N; ++i){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    for (const int& cards : arr){
+        cardsDequeASC.push(cards);
+    }
+    int ans = 0, cnt = 0, tmp = 0;
+    while(!cardsDequeASC.empty()){
+        ++cnt;
+        if(cnt%2==1){
+            tmp += cardsDequeASC.top();
+            cardsDequeASC.pop();
+        }
+        else{
+            tmp += cardsDequeASC.top();
+            cardsDequeASC.pop();
+            cardsDequeASC.push(tmp);
+            ans += tmp;
+            tmp = 0;
+        }
+    }
+    cout << ans;
+    return 0;
+}
